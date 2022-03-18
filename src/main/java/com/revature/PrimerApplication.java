ad1d9f80cdd8caf15f3237f4743b4d270f201c07
Anthony-Davis <adavi00@outlook.com>
2022-03-18 15:56:23 -0400

    fixing connection issues

--- src/main/java/com/revature/PrimerApplication.java
+++ src/main/java/com/revature/PrimerApplication.java
@@ -3,7 +3,7 @@ package com.revature;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.revature")
 public class PrimerApplication {
 	
 	public static void main(String[] args) {
